fd5cbfb816d935e95f2f5249b134fe16488e1ae5
“noarrassam” <noor.s.rassam@gmail.com>
2022-03-14 21:28:37 -0400

    createdLocalStorage

--- script.js
+++ script.js
@@ -14,12 +14,23 @@ function todoList() {
   delt.innerHTML = "Complete";
 
   add.addEventListener("keyup", function (e) {
-
     if (e.keyCode === 13) {
-        
+      items.innerHTML = add.value;
+      todo.push({ item: items.innerHTML });
+      console.log(todo);
+      let todoList = JSON.stringify(todo);
+      console.log(todoList);
+      localStorage.setItem("key", todoList);
+      let getItem = localStorage.getItem("key");
+      console.log(getItem);
+      let parsedObj = JSON.parse(getItem);
+      console.log(parsedObj);
+    }
+
+    for (const [key, value] of Object.entries(localStorage)) {
+      console.log(key, value);
     }
   });
 }
 
 todoList();
-
