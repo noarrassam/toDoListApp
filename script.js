8df1be6844c61d855b415119a6e2b8175fac4f6a
“noarrassam” <noor.s.rassam@gmail.com>
2022-03-17 13:42:30 -0400

    appendedDeleteButtonToLiTextContent

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ function todoList() {
   var items = document.createElement("li");
   var text = document.getElementById("text").value;
   items.innerHTML = text;
+
   todo.push(items.innerHTML);
   console.log(todo);
   localStorage.setItem("key", JSON.stringify(todo));
+  location.reload();
+}
+
+function getStorage() {
   if (localStorage.getItem("key") == null) {
     todo = [];
   } else {
     todo = JSON.parse(localStorage.getItem("key"));
-    for (let i = 0; i < todo.length; i++) {
-      items.innerHTML = todo[i];
-      document.getElementById("items").appendChild(items);
-    }
+    todo.forEach((item) => {
+      var li = document.createElement("li");
+      var delt = document.createElement("button");
+      delt.textContent = "Completed ";
+      var text = document.createTextNode(item);
+      li.appendChild(text);
+      li.appendChild(delt);
+      document.getElementById("items").appendChild(li);
+    });
   }
 }
 
-if (localStorage.getItem("key") == null) {
-  todo = [];
-} else {
-  todo = JSON.parse(localStorage.getItem("key"));
-  todo.forEach((item) => {
-    var li = document.createElement("li");
-    var text = document.createTextNode(item);
-    li.appendChild(text);
-    document.getElementById("items").appendChild(li);
-  });
-}
+getStorage();
