4d60119e4a3a8f76e4cafc5d20993c315859abb3
“noarrassam” <noor.s.rassam@gmail.com>
2022-03-17 12:21:10 -0400

    loopedOverTodoArrayAsLiElements

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 var todo = [];
+
+var addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", todoList);
+
 function todoList() {
-  var text = document.getElementById("text");
-  text.placeholder = "I need to...";
-
-  var items = document.getElementById("items");
-  var addBtn = document.getElementById("addBtn");
-
-  addBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    items.innerHTML = text.value + `<br/>`;
-    todo.push(items.innerHTML);
-    console.log(todo);
-    localStorage.setItem("key", JSON.stringify(todo));
-  });
-
+  var items = document.createElement("li");
+  var text = document.getElementById("text").value;
+  items.innerHTML = text;
+  todo.push(items.innerHTML);
+  console.log(todo);
+  localStorage.setItem("key", JSON.stringify(todo));
   if (localStorage.getItem("key") == null) {
     todo = [];
   } else {
     todo = JSON.parse(localStorage.getItem("key"));
-    items.innerHTML += `${todo} <br />`;
-    console.log(items);
+    for (let i = 0; i < todo.length; i++) {
+      items.innerHTML = todo[i];
+      document.getElementById("items").appendChild(items);
+    }
   }
 }
 
-todoList();
+if (localStorage.getItem("key") == null) {
+  todo = [];
+} else {
+  todo = JSON.parse(localStorage.getItem("key"));
+  todo.forEach((item) => {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("items").appendChild(li);
+  });
+}
